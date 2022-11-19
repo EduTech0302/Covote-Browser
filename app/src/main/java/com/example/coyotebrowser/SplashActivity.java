1ab812997ad3fc97486f8f56df2a3d8c39abcee3
dhanush678 <dhanugowda200326@gmail.com>
2022-11-19 18:00:05 +0530

    atill work remaining

---
+++ app/src/main/java/com/example/coyotebrowser/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.coyotebrowser;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.coyotebrowser.R;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.image_view);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,mainpage.class));
+                finish();
+            }
+        },130);
+    }
+}
